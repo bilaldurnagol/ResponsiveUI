7d5aa4e93a3bb30f729b1d3657baaefcf5c4180b
Bilal Durnagöl <bilal.durnagol.tt@gmail.com>
2022-09-21 10:22:48 +0300

    Initial Commit

---
+++ ResponsiveUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ResponsiveUI
+//
+//  Created by Bilal Durnagöl on 21.09.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ResponsiveUI/ResponsiveUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResponsiveUIApp.swift
+//  ResponsiveUI
+//
+//  Created by Bilal Durnagöl on 21.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct ResponsiveUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
